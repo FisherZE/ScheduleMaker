62f2cbed8fec230c05c6289a875f76d4af246527
jjyokup <128657754+jjyokup@users.noreply.github.com>
2023-05-05 08:03:56 +0000

    update

--- it327project/src/main/java/Schedule/ScheduleMaker.java
+++ it327project/src/main/java/Schedule/ScheduleMaker.java
@@ -1,35 +1,61 @@
 package Schedule;
-
 import java.time.*;
 import java.util.ArrayList;
 import Classes.Class;
  public class ScheduleMaker
  {
     private static ArrayList<Schedule> schedules;
+    private static ArrayList<DayOfWeek> eligibleDays;
     private static ArrayList<Event> events;
+    private static ArrayList<String> requiredCourses;
+    private static ArrayList<Class> classes;
     private static int minCreditHours;
     private static int maxCreditHours;
     private static int earliestTime;
     private static int latestTime;
+
+    // Starts up the schedulemaker. Stands in for a constructor
     public static void initialize()
     {
         schedules = new ArrayList<Schedule>();
         events = new ArrayList<Event>();
-        minCreditHours = 0;
+        eligibleDays = new ArrayList<DayOfWeek>(); 
+        classes = new ArrayList<Class>();
+        requiredCourses = new ArrayList<String>();
+        minCreditHours = 8;
         maxCreditHours = 30;
         earliestTime = 0;
         latestTime = 2400;
-    }
+        
+        
+    } 
 
-    public static void generateSchedules(ArrayList<Class> classList){
+    public static void generateSchedules(){
         //Creates a bitmask that is used to track the current combination of schedules being checked
         //A 1 in the bitmask signifies the presence of the class in that index in the current schedule
+        ArrayList<Class> classList = new ArrayList<Class>(classes);
+        ArrayList<Class> toRemove = new ArrayList<Class>();
+        initloop:
+        for (Class c: classList){
+            for (DayOfWeek day : c.getOnDays()){
+                if (!(eligibleDays.contains(day))){
+                    toRemove.add(c);
+                    continue initloop;
+                }else if (c.getStartTime() < earliestTime || c.getEndTime() > latestTime){
+                    toRemove.add(c);
+                    continue initloop;
+                }
+            }
+
+        }
+        for (Class c : toRemove){
+            classList.remove(c);
+        }
         int[] mask = new int[classList.size()];
         mainloop:
         for (int i = 0; i < Math.pow(2, mask.length); i++)
         {      
             Schedule newSched = new Schedule();
-            
             if (i != 0)
             {
                 mask[0]++;
@@ -46,17 +72,18 @@ import Classes.Class;
                     }
                 }
             }
-            // Checking for 1s which signifies a class
+            // Tracks how many required courses are included
+            // Will be referenced after the schedule is finalizied to see if it meets requirements
+            int includedRequirements = 0;
+
+            // Checking for 1s in the bitmask which signifies a class is included in the current schedule
             for (int j = 0; j < mask.length; j++)
             {
                 if (mask[j] == 1)
                 {
+                    //Reassigns the current class for easier access
                    Class cur = classList.get(j);
-                   // Checks to see if the included class meets user time preferences
-                   if (cur.getStartTime() < earliestTime || cur.getEndTime() > latestTime)
-                   {
-                    continue mainloop;
-                   }
+                 
                     for (Class c : newSched.getClasses()) 
                     {
                         // Checks to see if a class with the same identifier is alreadly present in the current schedule 
@@ -80,32 +107,98 @@ import Classes.Class;
                     // Checks for overlap with events
                     for (Event e : events) 
                     {
+                        System.out.print(cur.getOnDays());
                         for (DayOfWeek day : cur.getOnDays()) 
-                        {      
-                            if (e.getOnDays().contains(day))
+                        {    
+                            
+                            if (e.getgetOnDays().contains(day))
                             {
                                 if ((cur.getStartTime() >= e.getStartTime() && cur.getStartTime() <= e.getEndTime())||(cur.getEndTime() >= e.getStartTime() && cur.getEndTime() <= e.getEndTime()))
                                 {
+                                    System.out.println("event conflict");
                                     continue mainloop;
                                 }
 
                             }
                         }
-                    }             
+                    }       
+                    if (requiredCourses.contains(cur.getIdentifier())){
+                        includedRequirements++;
+                    }      
                     newSched.getClasses().add(cur);
                 }
             }
+            
             int cred = 0;
             for (Class c : newSched.getClasses()) 
             {
                 cred += c.getCreditHours();
             }
+            
             // Checks to see if the current schedule meets user credit hour preferences
-            if (cred >= minCreditHours && cred <= maxCreditHours)
+            
+            if (cred >= minCreditHours && cred <= maxCreditHours && includedRequirements == requiredCourses.size())
             {
                 schedules.add(newSched);
-            }                      
-        }           
+            }     
+                          
+        }        
+        
+    }
+    //Sorts the schedules by earliest start time
+    public static void sortSchedules(){
+        ArrayList<Schedule> sortedSchedules = new ArrayList<Schedule>();
+       
+        while (!schedules.isEmpty()){
+            int earlyTime = 2401;
+            int earlyIndex = 0;
+           for (int i = 0; i < schedules.size(); i++){
+            for (Class c : schedules.get(i).getClasses()){
+                if (c.getStartTime() < earlyTime){
+                    earlyTime = c.getStartTime();
+                    earlyIndex = i;
+                }
+
+            }
+           } 
+           sortedSchedules.add(schedules.get(earlyIndex));
+           schedules.remove(earlyIndex);
+        }
+        schedules = sortedSchedules;
+        
+
+    }
+    // Checks to see which class has the most conflictions. Returns null if no culprit is found
+    public static String courseConflicts(){
+        int maxConflict = 0;
+        Class conflictingClass = null;
+        boolean tie = false;
+        for (Class c : classes){
+            int conflict = 0;
+            for (Class c2 : classes){
+                if (!c2.getIdentifier().equals(c.getIdentifier())){
+                    if ((c.getStartTime() >= c2.getStartTime() && c.getStartTime() <= c2.getEndTime())||(c.getEndTime() >= c2.getStartTime() && c.getEndTime() <= c2.getEndTime())){
+                        conflict++;
+                    }
+                }
+                if (conflict > maxConflict){
+                    maxConflict = conflict;
+                    tie = false;
+                    conflictingClass = c;
+
+                }else if(conflict == maxConflict){
+                    tie = true;
+
+                }
+            }
+
+        }
+        if (conflictingClass != null){
+            return conflictingClass.toString();
+        }else{
+            return null;
+        }
+        
     }
 
     public static void setMinCreditHours(int hours)
@@ -124,10 +217,12 @@ import Classes.Class;
     {
         latestTime = time;
     }
+
     public static ArrayList<Schedule> getSchedules()
     {
         return schedules;
     }
+
     public static void addEvent(Event e)
     {
         events.add(e);
@@ -137,5 +232,27 @@ import Classes.Class;
             return events.remove(e);
     }
     
+    public static void addEligibleDay(DayOfWeek day){
+        eligibleDays.add(day);
+
+    }
+    public static void removeEligibleDay(DayOfWeek day){
+        eligibleDays.remove(day);
+    }
+
+    public static void addRequiredCourse(String name){
+        requiredCourses.add(name);
+    }
+    public static void removeRequiredCourse(String name){
+        requiredCourses.remove(name);
+    }
+
+    public static void addClass(Class c){
+        classes.add(c);
+    }
+    public static void removeClass(Class c){
+        classes.remove(c);
+    }
+
 
  }

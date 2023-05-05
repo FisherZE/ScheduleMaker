c42fdffb361935ebb0f483fbb7d1d92daf6852ce
jjyokup <128657754+jjyokup@users.noreply.github.com>
2023-05-05 08:48:18 +0000

    Pending changes exported from your codespace

--- it327project/src/main/java/Controllers/ExportController.java
+++ it327project/src/main/java/Controllers/ExportController.java
@@ -1,5 +1,27 @@
 package Controllers;
-
+import java.text.BreakIterator;
+import java.time.DayOfWeek;
 public class ExportController {
-    
+    public static String toDate(DayOfWeek d){
+        switch (d){
+            MONDAY: return "20230501";
+            break;
+            TUESDAY:
+            break;
+            WEDNESDAY:
+            break;
+            THURSDAY:
+            break;
+            FRIDAY:
+            break;
+            SATURDAY:
+            break;
+            SUNDAY:
+            break;
+
+        }
+
+        
+
+    }
 }
